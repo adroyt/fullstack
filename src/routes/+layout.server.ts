0bc081996f36f2d4f1019a0c6b285c695c1e043f
Haniel Ubogu <hanieltobi@gmail.com>
2022-09-07 17:21:02 +0100

    ran prettier and updated layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,3 +1,12 @@
 import { auth } from "$lib/lucia";
+import type { LayoutServerLoad } from "./$types";
 
-export const load = auth.load;
+export const load: LayoutServerLoad = async event => {
+  // here so that this function reloads on url change
+  // that way using `goto` can reload the browser cookies
+  event.url.pathname;
+
+  const loadData = await auth.load(event);
+
+  return { ...loadData };
+};
