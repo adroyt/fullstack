b615dd8bc5b2317f1fa6f90fe1d808e4728c1dde
Haniel Ubogu <hanieltobi@gmail.com>
2022-09-13 07:24:14 +0100

    updated trpc template

--- src/lib/trpc/index.ts
+++ src/lib/trpc/index.ts
@@ -1,15 +1,4 @@
-import * as trpc from "@trpc/server";
-import type { inferAsyncReturnType } from "@trpc/server";
-import type { RequestEvent } from "@sveltejs/kit";
-
-export const createContext = async (opts: RequestEvent) => {
-  // console.log(opts.setHeaders);
-  return { ...opts };
-};
-
-export const createRouter = () => {
-  return trpc.router<inferAsyncReturnType<typeof createContext>>();
-};
+import { createRouter } from "./app-router";
 
 export const appRouter = createRouter();
 
