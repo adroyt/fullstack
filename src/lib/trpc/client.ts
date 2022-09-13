32537a9ff2b4ee056f1e5a1c372978b4a471acf7
Haniel Ubogu <hanieltobi@gmail.com>
2022-09-13 07:15:36 +0100

    updated deps

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -1,12 +1,12 @@
-import { browser } from "$app/environment";
+import * as trpc from "@trpc/client";
 import type { AppRouter } from "$trpc"; // 👈 only the types are imported from the server
 import type { LoadEvent } from "@sveltejs/kit";
-import * as trpc from "@trpc/client";
+import { browser } from "$app/environment";
 
 const url = browser ? "/trpc" : "http://localhost:3000/trpc";
 
 export const trpcClient = (loadFetch?: LoadEvent["fetch"]) =>
   trpc.createTRPCClient<AppRouter>({
     url: loadFetch ? "/trpc" : url,
-    ...(loadFetch && { fetch: loadFetch as typeof fetch })
+    ...(loadFetch && { fetch: loadFetch as typeof fetch }),
   });
