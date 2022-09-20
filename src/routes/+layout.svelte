e7675ea18cfa88a4e8b20c4ea2262e8f300000f3
Haniel Ubogu <hanieltobi@gmail.com>
2022-09-20 19:17:29 +0100

    fixed auth bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import "../app.css";
-  import { handleSilentRefresh } from "lucia-sveltekit/client";
-  import { page } from "$app/stores";
+  import { getSession, handleSilentRefresh } from "lucia-sveltekit/client";
   import { session } from "$lib/stores/user";
 
   handleSilentRefresh();
-  $: session.set($page.data.session);
+  const luciaSession = getSession();
+  session.set($luciaSession);
 </script>
 
-<!-- Updates UI based on auth state -->
-{#key $session}
-  <slot />
-{/key}
+<slot />
